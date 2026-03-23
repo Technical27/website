f1ad465c45f8a7a1796437be2d6e544032d51027
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2026-03-22 23:52:16 -0400

    fix more bug in timezone guessing

--- static/index.js
+++ static/index.js
@@ -2,14 +2,13 @@
 
 const local = luxon.DateTime.now();
 
-const IDN_TIME = ["Asia/Ujung_Pandang", "Asia/Pontianak", "Asia/Makassar", "Asia/Jayapura", "Asia/Jakarta"].map(luxon.IANAZone.create);
 const list = document.getElementById("fun-list");
 
-if (IDN_TIME.some(tz => local.zone.equals(tz))) {
+if (local.offset === 420) {
   list.innerHTML += "<li>hi bramble</li>";
 } else if (local.zone.ianaName.startsWith("Australia")) {
   list.innerHTML += "<li>Hi to you all in Australia!</li>";
-} else if (local.zone.ianaName === "Asia/Manila") {
+} else if (local.offset === 480) {
   list.innerHTML += "<li>website programming isn't easy ok</li>";
 } else {
   const est = local.setZone("America/New_York");
