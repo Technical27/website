9463115f0ad648edd522c3671ffd564f8a8f70f6
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2026-04-21 14:27:58 -0400

    more sweeping page changes

--- static/index.js
+++ static/index.js
@@ -1,43 +1,81 @@
 "use strict";
 
-const local = luxon.DateTime.now();
+const headingText = "Aamaruvi";
+const subheadingText = "hi. and welcome.";
+const paraText = "i can't be here for too long.\nfind me elsewhere to truly know me.\nthis interface may\rchange and reconfigure at only my will.\nthe hyperlinks are all\ri can give you right now.\nmore to come when i am able.";
 
-const list = document.getElementById("fun-list");
-const messages = [];
+const h1 = document.getElementById("title");
+const h2 = document.getElementById("subtitle");
+const para = document.getElementById("para");
 
-// Indonesia and the Philippines both have distintive timezones, but for some stupid reason,
-// windows groups all the timezones for these regions with many others despite not being the same at all.
-// just because this is a really really stupid funny joke, just use UTC offsets since both places do not observe DST.
+const links = [["GOTO", document.getElementById("link1")], ["JMP", document.getElementById("link2")],
+["window.location.href =", document.getElementById("link3")], ["wget", document.getElementById("link4")], ["curl", document.getElementById("link5")]];
 
-// UTC+7
-if (local.offset === 420) {
-  messages.push("hi bramble");
-// UTC+8
-} else if (local.offset === 480) {
-  messages.push("website programming isn't easy ok");
-// Check if time is the same as New York, easiest way to account for DST differences
-} else {
-  const est = local.setZone("America/New_York");
-  if (est.offset === local.offset) {
-    if (est.isInDST) {
-      messages.push("Eastern Standard Time gang, daylight edition");
+const genChar = () => {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890*&^$#@!()'\"[]{}-_=+;;,.<>/?`~\\|";
+  return chars.charAt(Math.random() * chars.length);
+}
+
+const decText = (el, text, idx) => {
+  let b = [];
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == '\r') {
+      if (window.innerWidth < 520) {
+        b.push('\n');
+      } else {
+        b.push(' ');
+      }
+    } else if (text.charAt(i) == '\n') {
+      b.push('\n');
+    } else if (i < idx) {
+      b.push(text.charAt(i));
     } else {
-      messages.push("Eastern Standard Time gang");
+      b.push(genChar());
     }
   }
+
+  el.innerText = b.join('');
 }
 
-// I am far too lazy to check every single Australia timezone, just hope this is good enough unless windows is stupid again
-if (local.zone.ianaName.startsWith("Australia")) {
-  messages.push("Hi to you all in Australia!");
-}
+let dt = 1;
+let done = false;
+const decLoop = () => {
+  if (dt < paraText.length + 32) {
+    setTimeout(() => {
+      decText(h1, headingText, dt);
+      decText(h2, subheadingText, dt);
+      decText(para, paraText, dt);
+      for (const [text, el] of links) {
+        decText(el, text, dt);
+      }
+      for (const el of document.getElementsByTagName("a")) {
+        //el.innerText = el.getAttribute("href");
+        decText(el, el.getAttribute("href"), dt);
+      }
+      if (dt > paraText.length / 2) {
+        dt += 32;
+      } else if (dt > subheadingText.length) {
+        dt += 16;
+      } else if (dt > headingText.length) {
+        dt += 8;
+      } else {
+        dt += 1;
+      }
+      decLoop();
+    }, 60);
+  } else {
+    done = true;
+  }
+};
 
-if (local.offset % 60 !== 0) {
-  messages.push("Thank you for being an example of why UTC offsets are a bad way to measure time. Your current UTC offset is " + local.toFormat("ZZ"))
-}
+window.addEventListener("resize", () => {
+  if (done) decText(para, paraText, dt);
+})
 
-if (local.hour > 2 && local.hour < 6) {
-  messages.push("<li>you should go to sleep its " + local.toLocaleString(luxon.DateTime.TIME_SIMPLE) + "</li>");
-}
+/* setTimeout(() => {
+  for (const el of document.getElementsByTagName("a")) {
+    el.innerText = el.getAttribute("href");
+  }
+}, 60); */
 
-list.innerHTML += messages.map(x => `<li>${x}</li>`).join('\n')
+decLoop();
